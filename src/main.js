5cad74f578ec87e3b2e1a28f484474579294112a
Jack Dunning <JackStunning9001@gmail.com>
2020-02-14 09:39:16 -0800

    Working on api call

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import $ from 'jquery';
+import { GetEndPoint } from './d&d-db';
+
+const url = "http://www.dnd5eapi.co/api/";
+
+function getData() {
+
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let newData = data;
+      console.log("newData"+JSON.stringify(newData));
+      console.log(newData);
+      return newData;
+    });
+}
+
+$(document).ready(function(){
+  let data = getData();
+  let endPoint = new GetEndPoint();
+  console.log("endPoint"+JSON.stringify(endPoint));
+  console.log("jqueryData"+data);
+  $("#data-div").text(data);
+  $("#data-div").text("hello");
+});
